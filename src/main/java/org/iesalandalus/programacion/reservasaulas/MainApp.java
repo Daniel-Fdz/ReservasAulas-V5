2f0678508c5e8e268ec71acb6942c2fc9cc5e2bf
Daniel-Fdz <pure_code@outlook.com>
2022-06-04 21:48:35 +0200

    Cambiado el MainApp para que pueda hacer uso de MongoDB

--- src/main/java/org/iesalandalus/programacion/reservasaulas/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/reservasaulas/MainApp.java
@@ -3,6 +3,7 @@ package org.iesalandalus.programacion.reservasaulas;
 import org.iesalandalus.programacion.reservasaulas.mvc.controlador.Controlador;
 import org.iesalandalus.programacion.reservasaulas.mvc.controlador.IControlador;
 import org.iesalandalus.programacion.reservasaulas.mvc.modelo.FactoriaFuenteDatos;
+import org.iesalandalus.programacion.reservasaulas.mvc.modelo.IFuenteDatos;
 import org.iesalandalus.programacion.reservasaulas.mvc.modelo.IModelo;
 import org.iesalandalus.programacion.reservasaulas.mvc.modelo.Modelo;
 import org.iesalandalus.programacion.reservasaulas.mvc.vista.FactoriaVista;
@@ -11,7 +12,7 @@ import org.iesalandalus.programacion.reservasaulas.mvc.vista.IVista;
 public class MainApp {
 
     public static void main(String[] args) {
-        IModelo modelo = new Modelo(FactoriaFuenteDatos.FICHEROS.crear());
+        IModelo modelo = new Modelo(argumentosDatos(args));
         IVista vista = argumentosVista(args);
         IControlador controlador = new Controlador(modelo, vista);
         controlador.comenzar();
@@ -27,6 +28,20 @@ public class MainApp {
             }
         }
         return vista;
+    }
+    
+    private static IFuenteDatos argumentosDatos(String[] args) {
+        IFuenteDatos fuenteDatos = FactoriaFuenteDatos.MONGODB.crear();
+        for (String argumento : args) {
+            if (argumento.equalsIgnoreCase("-dMongo") ) {
+            	fuenteDatos = FactoriaFuenteDatos.MONGODB.crear();
+            } else if (argumento.equalsIgnoreCase("-dMemoria")) {
+            	fuenteDatos = FactoriaFuenteDatos.MEMORIA.crear();
+            } else if (argumento.equalsIgnoreCase("-dFicheros")) {
+            	fuenteDatos = FactoriaFuenteDatos.FICHEROS.crear();
+            }
+        }
+        return fuenteDatos;
     }
 
 }
